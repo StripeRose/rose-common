62b01ed495191fd353a608c495a2f719438bc371
Stripe Rose <stripe@striperose.se>
2024-10-06 15:03:20 +0200

    Rename enum-flags define to not interfere with Win32's version.

--- include/rose-common/Enum.hpp
+++ include/rose-common/Enum.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#ifndef DEFINE_ENUM_FLAG_OPERATORS
+#ifndef ENUM_FLAGS
 
 	#include <type_traits>
 
-	#define DEFINE_ENUM_FLAG_OPERATORS(EnumType) \
+	#define ENUM_FLAGS(EnumType) \
 	inline constexpr EnumType operator|(EnumType a, EnumType b) noexcept { return (EnumType)((std::underlying_type_t<EnumType>)a | (std::underlying_type_t<EnumType>)b); } \
 	inline EnumType& operator|=(EnumType& a, EnumType b) noexcept { return (EnumType&)(((std::underlying_type_t<EnumType>&)a) |= ((std::underlying_type_t<EnumType>)b)); } \
 	inline constexpr EnumType operator&(EnumType a, EnumType b) noexcept { return EnumType(((std::underlying_type_t<EnumType>)a) & ((std::underlying_type_t<EnumType>)b)); } \
