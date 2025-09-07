add7ca17388df4dad4c3e6b2b24b1ab8631b2233
Stripe Rose <stripe@striperose.se>
2025-09-07 22:55:23 +0200

    Add ability for EventSlot to disconnect by callback Id

--- include/rose-common/EventSlot.hpp
+++ include/rose-common/EventSlot.hpp
@@ -7,6 +7,20 @@
 
 namespace ROSECOMMON_NAMESPACE
 {
+	struct EventCallbackID
+	{
+	private:
+		template <typename ... T>
+		friend class EventSlot;
+
+		EventCallbackID(const void* aSlot, int aCallbackId)
+			: myEventSlot(aSlot), myCallbackId(aCallbackId)
+		{ }
+
+		const void* myEventSlot;
+		int myCallbackId;
+	};
+
 	/**
 	 * @brief An interface for defining events which other objects can listen for the invokation of.
 	 * @tparam ...CallbackArguments A list of argument types the event must use.
@@ -15,42 +29,78 @@ namespace ROSECOMMON_NAMESPACE
 	class EventSlot
 	{
 	public:
+		EventSlot() = default;
+		~EventSlot() = default;
+
+		// Shouldn't copy callbacks.
+		EventSlot(const EventSlot&) = delete;
+		EventSlot& operator=(const EventSlot&) = delete;
+
+		// Can't move callbacks and maintain callback IDs.
+		EventSlot(EventSlot&&) = delete;
+		EventSlot& operator=(EventSlot&&) = delete;
+
 		/**
 		 * @brief Invoke all currently connected functions.
 		 * @param ...someArguments A list of arguments to call the connected functions with.
 		 */
 		void Invoke(CallbackArguments... someArguments) const
 		{
-			for (const auto& callback : myCallbacks)
+			for (auto it : myCallbacks)
 			{
-				if (callback.second)
-					callback.second(someArguments...);
+				if (it.second)
+					it.second(someArguments...);
 			}
+		}
+
+		/**
+		 * @brief Register new function to be called when the EventSlot is invoked.
+		 * @param aFunction A function to connect.
+		 * @return A callback id to use when unregistering.
+		 */
+		EventCallbackID Connect(std::function<void(CallbackArguments...)> aFunction)
+		{
+			myCallbacks.emplace(myRollingId, aFunction);
+			return EventCallbackID{ this, myRollingId++ };
 		}
 
 		/**
 		 * @brief Register new function to be called when the EventSlot is invoked.
 		 * @param aRegistrar A pointer to attribute the connection to.
 		 * @param aFunction A function to connect.
-		 * @return Whether the operation was successful.
+		 * @return A callback id to use when unregistering.
 		 */
-		bool Connect(void* aRegistrar, std::function<void(CallbackArguments...)> aFunction)
+		EventCallbackID Connect(const void* aRegistrar, std::function<void(CallbackArguments...)> aFunction)
 		{
-			if (myCallbacks.contains(aRegistrar))
-				return false;
-
-			myCallbacks[aRegistrar] = aFunction;
-			return true;
+			myCallbacks.emplace(myRollingId, aFunction);
+			myRegistrarCallbacks[aRegistrar].push_back(myRollingId);
+			return EventCallbackID{ this, myRollingId++ };
 		}
 
 		/**
-		 * @brief Unregister a function from the specified registrar.
-		 * @param aRegistrar A registrar to disconnect the function from.
+		 * @brief Unregister a specific callback.
+		 * @param aCallbackId A callback id given when registering.
 		 */
-		void Disconnect(void* aRegistrar)
+		void Disconnect(const EventCallbackID& aCallbackId)
 		{
-			if (myCallbacks.contains(aRegistrar))
-				myCallbacks.at(aRegistrar) = nullptr;
+			if (aCallbackId.myEventSlot == this)
+				myCallbacks.erase(aCallbackId.myCallbackId);
+		}
+
+		/**
+		 * @brief Unregister all functions from a specified registrar.
+		 * @param aRegistrar A registrar whose functions should be disconnected.
+		 */
+		void Disconnect(const void* aRegistrar)
+		{
+			if (!myRegistrarCallbacks.contains(aRegistrar))
+				return;
+
+			const std::vector<int>& callbacks = myRegistrarCallbacks[aRegistrar];
+			for (int id : callbacks)
+				myCallbacks.erase(id);
+
+			myRegistrarCallbacks.erase(aRegistrar);
 		}
 
 		void operator()(CallbackArguments... someArguments) const
@@ -59,7 +109,9 @@ namespace ROSECOMMON_NAMESPACE
 		}
 
 	private:
-		std::map<void*, std::function<void(CallbackArguments...)>> myCallbacks;
+		int myRollingId = 0;
+		std::map<int, std::function<void(CallbackArguments...)>> myCallbacks;
+		std::map<const void*, std::vector<int>> myRegistrarCallbacks;
 	};
 
 	/**
@@ -69,41 +121,77 @@ namespace ROSECOMMON_NAMESPACE
 	class EventSlot<void>
 	{
 	public:
+		EventSlot() = default;
+		~EventSlot() = default;
+
+		// Shouldn't copy callbacks.
+		EventSlot(const EventSlot&) = delete;
+		EventSlot& operator=(const EventSlot&) = delete;
+
+		// Can't move callbacks and maintain callback IDs.
+		EventSlot(EventSlot&&) = delete;
+		EventSlot& operator=(EventSlot&&) = delete;
+
 		/**
 		 * @brief Invoke all currently connected functions.
 		 */
 		void Invoke() const
 		{
-			for (const auto& callback : myCallbacks)
+			for (auto it : myCallbacks)
 			{
-				if (callback.second)
-					callback.second();
+				if (it.second)
+					it.second();
 			}
+		}
+
+		/**
+		 * @brief Register new function to be called when the EventSlot is invoked.
+		 * @param aFunction A function to connect.
+		 * @return A callback id to use when unregistering.
+		 */
+		EventCallbackID Connect(std::function<void()> aFunction)
+		{
+			myCallbacks.emplace(myRollingId, aFunction);
+			return EventCallbackID{ this, myRollingId++ };
 		}
 
 		/**
 		 * @brief Register new function to be called when the EventSlot is invoked.
 		 * @param aRegistrar A pointer to attribute the connection to.
 		 * @param aFunction A function to connect.
-		 * @return Whether the operation was successful.
+		 * @return A callback id to use when unregistering.
 		 */
-		bool Connect(void* aRegistrar, std::function<void()> aFunction)
+		EventCallbackID Connect(const void* aRegistrar, std::function<void()> aFunction)
 		{
-			if (myCallbacks.contains(aRegistrar))
-				return false;
-
-			myCallbacks[aRegistrar] = aFunction;
-			return true;
+			myCallbacks.emplace(myRollingId, aFunction);
+			myRegistrarCallbacks[aRegistrar].push_back(myRollingId);
+			return EventCallbackID{ this, myRollingId++ };
 		}
 
 		/**
-		 * @brief Unregister a function from the specified registrar.
-		 * @param aRegistrar A registrar to disconnect the function from.
+		 * @brief Unregister a specific callback.
+		 * @param aCallbackId A callback id given when registering.
 		 */
-		void Disconnect(void* aRegistrar)
+		void Disconnect(const EventCallbackID& aCallbackId)
 		{
-			if (myCallbacks.contains(aRegistrar))
-				myCallbacks.at(aRegistrar) = nullptr;
+			if (aCallbackId.myEventSlot == this)
+				myCallbacks.erase(aCallbackId.myCallbackId);
+		}
+
+		/**
+		 * @brief Unregister all functions from a specified registrar.
+		 * @param aRegistrar A registrar whose functions should be disconnected.
+		 */
+		void Disconnect(const void* aRegistrar)
+		{
+			if (!myRegistrarCallbacks.contains(aRegistrar))
+				return;
+
+			const std::vector<int>& callbacks = myRegistrarCallbacks[aRegistrar];
+			for (int id : callbacks)
+				myCallbacks.erase(id);
+
+			myRegistrarCallbacks.erase(aRegistrar);
 		}
 
 		void operator()() const
@@ -112,6 +200,8 @@ namespace ROSECOMMON_NAMESPACE
 		}
 
 	private:
-		std::map<void*, std::function<void()>> myCallbacks;
+		int myRollingId = 0;
+		std::map<int, std::function<void()>> myCallbacks;
+		std::map<const void*, std::vector<int>> myRegistrarCallbacks;
 	};
 }
--- tests/Test_EventSlot.hpp
+++ tests/Test_EventSlot.hpp
@@ -5,12 +5,13 @@
 #include <catch2/generators/catch_generators_random.hpp>
 
 #include <array>
+#include <bitset>
 
 namespace
 {
-	SCENARIO("An event slot with no arguments can be defined empty or as void")
+	SCENARIO("An EventSlot with no arguments can be defined empty or as void")
 	{
-		GIVEN("One event slot of each type")
+		GIVEN("One EventSlot of each type")
 		{
 			RoseCommon::EventSlot<> emptySlot;
 			RoseCommon::EventSlot<void> voidSlot;
@@ -23,15 +24,15 @@ namespace
 		}
 	}
 
-	SCENARIO("Connecting to and invoking an event slot")
+	SCENARIO("Connecting to and invoking an EventSlot")
 	{
 		// Todo: Can this be rewritten to be generalized no matter the amount of arguments?
 
-		GIVEN("An event slot with no arguments and one connection") // Should equal case with void argument
+		GIVEN("An EventSlot with no arguments and one connection") // Should equal case with void argument
 		{
 			RoseCommon::EventSlot<> slot;
 			bool wasInvoked = false;
-			slot.Connect(nullptr, [&wasInvoked]() { wasInvoked = true; });
+			slot.Connect([&wasInvoked]() { wasInvoked = true; });
 
 			WHEN("Invoking it will run the connected functions")
 			{
@@ -41,11 +42,11 @@ namespace
 			}
 		}
 
-		GIVEN("An event slot with void argument and one connection") // Should equal case with no arguments
+		GIVEN("An EventSlot with void argument and one connection") // Should equal case with no arguments
 		{
 			RoseCommon::EventSlot<void> slot;
 			bool wasInvoked = false;
-			slot.Connect(nullptr, [&wasInvoked]() { wasInvoked = true; });
+			slot.Connect([&wasInvoked]() { wasInvoked = true; });
 
 			WHEN("Invoking it will run the connected functions")
 			{
@@ -55,11 +56,11 @@ namespace
 			}
 		}
 
-		GIVEN("An event slot with one reference argument and one connection")
+		GIVEN("An EventSlot with one reference argument and one connection")
 		{
 			RoseCommon::EventSlot<int&> slot;
 			int invokeCount = 0;
-			slot.Connect(nullptr, [](int& anInvokeCount) { anInvokeCount += 1; });
+			slot.Connect([](int& anInvokeCount) { anInvokeCount += 1; });
 
 			WHEN("Invoking it will run the connected functions")
 			{
@@ -69,13 +70,13 @@ namespace
 			}
 		}
 
-		GIVEN("An event slot with two arguments and one connection")
+		GIVEN("An EventSlot with two arguments and one connection")
 		{
 			static constexpr const char* TestString = "A testing string";
 
 			RoseCommon::EventSlot<int&, const char*> slot;
 			int invokeCount = 0;
-			slot.Connect(nullptr, [](int& anInvokeCount, const char* aString) {
+			slot.Connect([](int& anInvokeCount, const char* aString) {
 				CHECK(std::strcmp(aString, TestString) == 0);
 				anInvokeCount += 1;
 				});
@@ -89,79 +90,114 @@ namespace
 		}
 	}
 
-	SCENARIO("Connecting and disconnecting with multiple registrars for the same slot")
+	SCENARIO("Connecting multiple callbacks")
 	{
-		GIVEN("An event slot with connections from multiple registrars")
+		GIVEN("An EventSlot with callbacks from multiple registrars, where each may have multiple callbacks")
 		{
 			RoseCommon::EventSlot<> slot;
 
-			// The invoke count addresses are also used as fake registrars for this test.
-			std::array<int, 5> invokeCounts;
-			invokeCounts.fill(0);
+			// Integers used as stand-ins for registrars.
+			int registrarA = 0;
+			int registrarB = 0;
 
-			slot.Connect(&invokeCounts[0], [&invokeCounts]() { invokeCounts[0] += 1; });
-			slot.Connect(&invokeCounts[1], [&invokeCounts]() { invokeCounts[1] += 1; });
-			slot.Connect(&invokeCounts[2], [&invokeCounts]() { invokeCounts[2] += 1; });
-			// invokeCounts[3] does not have a connection and thus should stay at zero.
-			slot.Connect(&invokeCounts[4], [&invokeCounts]() { invokeCounts[4] += 1; });
+			std::bitset<6> invokeBits;
+			const RoseCommon::EventCallbackID a = slot.Connect(&registrarA, [&invokeBits]() { invokeBits.set(0); });
+			const RoseCommon::EventCallbackID b = slot.Connect(&registrarA, [&invokeBits]() { invokeBits.set(1); });
+			const RoseCommon::EventCallbackID c = slot.Connect(&registrarB, [&invokeBits]() { invokeBits.set(2); });
+			const RoseCommon::EventCallbackID d = slot.Connect(&registrarB, [&invokeBits]() { invokeBits.set(3); });
+			const RoseCommon::EventCallbackID e = slot.Connect([&invokeBits]() { invokeBits.set(4); });
+			const RoseCommon::EventCallbackID f = slot.Connect([&invokeBits]() { invokeBits.set(5); });
 
 			WHEN("Invoking the slot")
 			{
 				slot.Invoke();
 
-				THEN("Each connected callback was run sequentially (in any order)")
+				THEN("All callbacks are run")
 				{
-					CHECK(invokeCounts[0] == 1);
-					CHECK(invokeCounts[1] == 1);
-					CHECK(invokeCounts[2] == 1);
-					CHECK(invokeCounts[3] == 0);
-					CHECK(invokeCounts[4] == 1);
+					CHECK(invokeBits.test(0));
+					CHECK(invokeBits.test(1));
+					CHECK(invokeBits.test(2));
+					CHECK(invokeBits.test(3));
+					CHECK(invokeBits.test(4));
+					CHECK(invokeBits.test(5));
 				}
 			}
 
-			WHEN("Disconnecting a specific registrar and then invoking the slot")
+			WHEN("Disconnecting by registrar")
 			{
-				slot.Disconnect(&invokeCounts[1]);
-				slot.Invoke();
+				slot.Disconnect(&registrarB);
 
-				THEN("No callback registered by the registrar has been run")
+				AND_THEN("Invoking the slot")
 				{
-					CHECK(invokeCounts[0] == 1);
-					CHECK(invokeCounts[1] == 0);
-					CHECK(invokeCounts[2] == 1);
-					CHECK(invokeCounts[3] == 0);
-					CHECK(invokeCounts[4] == 1);
+					slot.Invoke();
+
+					THEN("Only the remaining connected callbacks are run")
+					{
+						CHECK(invokeBits.test(0));
+						CHECK(invokeBits.test(1));
+						CHECK(!invokeBits.test(2));
+						CHECK(!invokeBits.test(3));
+						CHECK(invokeBits.test(4));
+						CHECK(invokeBits.test(5));
+					}
+				}
+			}
+
+			WHEN("Disconnecting by IDs")
+			{
+				slot.Disconnect(b);
+				slot.Disconnect(c);
+				slot.Disconnect(e);
+
+				AND_THEN("Invoking the slot")
+				{
+					slot.Invoke();
+
+					THEN("Only the remaining connected callbacks are run")
+					{
+						CHECK(invokeBits.test(0));
+						CHECK(!invokeBits.test(1));
+						CHECK(!invokeBits.test(2));
+						CHECK(invokeBits.test(3));
+						CHECK(!invokeBits.test(4));
+						CHECK(invokeBits.test(5));
+					}
 				}
 			}
 		}
 	}
 
-	SCENARIO("Connecting multiple callbacks from the same registrar to a single slot")
+	SCENARIO("Moving and copying EventCallbackID structures")
 	{
-		GIVEN("An event slot with no arguments and one connection")
-		{
-			RoseCommon::EventSlot<> slot;
-			bool wasInvoked = false;
-			CHECK(slot.Connect(nullptr, [&wasInvoked]() { wasInvoked = true; }));
+		// Here to make sure common operations don't become unavailable.
 
-			WHEN("Connecting a second callback")
-			{
-				const bool wasSucceessful = slot.Connect(nullptr, []() {});
-				THEN("The connection fails")
-				{
-					CHECK(!wasSucceessful);
-				}
-			}
-		}
+		std::vector<RoseCommon::EventCallbackID> callbacks;
+
+		RoseCommon::EventSlot<> slot1;
+		RoseCommon::EventSlot<int> slot2;
+
+		callbacks.emplace_back(slot1.Connect([]() {}));
+		callbacks.push_back(slot2.Connect([](int) {}));
+
+		const auto id1_2 = slot1.Connect([]() {});
+		callbacks.emplace_back(id1_2);
+		callbacks.push_back(id1_2);
+
+		RoseCommon::EventCallbackID id2_2 = slot2.Connect([](int) {});
+		RoseCommon::EventCallbackID id2_2copy = id2_2;
+		callbacks.push_back(std::move(id2_2));
+
+		// Original should remain the same even after the move to prevent creating an invalid ID.
+		CHECK(std::memcmp(&id2_2, &id2_2copy, sizeof(id2_2)) == 0);
 	}
 
 	SCENARIO("Invoking a slot via named method or operator")
 	{
-		GIVEN("An event slot with no arguments and one connection")
+		GIVEN("An EventSlot with no arguments and one connection")
 		{
 			RoseCommon::EventSlot<> slot;
 			bool wasInvoked = false;
-			slot.Connect(nullptr, [&wasInvoked]() { wasInvoked = true; });
+			slot.Connect([&wasInvoked]() { wasInvoked = true; });
 
 			WHEN("Invoking it with the named method")
 			{
