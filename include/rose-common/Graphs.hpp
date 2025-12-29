f0e6b5f8ddf61058e1f48dc54f79a5b478fa61cc
Stripe Rose <stripe@striperose.se>
2025-12-29 13:23:46 +0100

    Change some indentation to tabs

--- include/rose-common/Graphs.hpp
+++ include/rose-common/Graphs.hpp
@@ -15,35 +15,35 @@ namespace ROSECOMMON_NAMESPACE
 	{
 		// Sourced from: https://biowpn.github.io/bioweapon/2023/11/03/topological-sort.html
 
-        const std::size_t n = std::ranges::distance(aFirst, aLast);
-        std::vector<std::size_t> in_degree(n);
+		const std::size_t n = std::ranges::distance(aFirst, aLast);
+		std::vector<std::size_t> in_degree(n);
 
-        for (std::size_t i = 0; i < n; ++i) {
-            for (std::size_t j = 0; j < n; ++j) {
-                in_degree[i] += bool(aHasEdge(aFirst[j], aFirst[i]));
-            }
-        }
+		for (std::size_t i = 0; i < n; ++i) {
+			for (std::size_t j = 0; j < n; ++j) {
+				in_degree[i] += bool(aHasEdge(aFirst[j], aFirst[i]));
+			}
+		}
 
-        // [s_first, s_last) are the sources of the sub-graph [s_first, last)
-        auto s_first = aFirst;
-        auto s_last = s_first;
+		// [s_first, s_last) are the sources of the sub-graph [s_first, last)
+		auto s_first = aFirst;
+		auto s_last = s_first;
 
-        for (std::size_t i = 0; i < n; ++i) {
-            if (in_degree[i] == 0) {
-                std::swap(aFirst[i], *s_last);
-                std::swap(in_degree[i], in_degree[s_last - aFirst]);
-                ++s_last;
-            }
-        }
+		for (std::size_t i = 0; i < n; ++i) {
+			if (in_degree[i] == 0) {
+				std::swap(aFirst[i], *s_last);
+				std::swap(in_degree[i], in_degree[s_last - aFirst]);
+				++s_last;
+			}
+		}
 
-        for (; s_first != s_last; ++s_first) {
-            for (auto t_it = s_last; t_it != aLast; ++t_it) {
-                if (aHasEdge(*s_first, *t_it) && --in_degree[t_it - aFirst] == 0) {
-                    std::swap(*t_it, *s_last);
-                    std::swap(in_degree[t_it - aFirst], in_degree[s_last - aFirst]);
-                    ++s_last;
-                }
-            }
-        }
+		for (; s_first != s_last; ++s_first) {
+			for (auto t_it = s_last; t_it != aLast; ++t_it) {
+				if (aHasEdge(*s_first, *t_it) && --in_degree[t_it - aFirst] == 0) {
+					std::swap(*t_it, *s_last);
+					std::swap(in_degree[t_it - aFirst], in_degree[s_last - aFirst]);
+					++s_last;
+				}
+			}
+		}
 	}
 }
