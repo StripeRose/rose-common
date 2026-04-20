0677e8387fd533c2e402361b8d32768adababb9a
Stripe Rose <stripe@striperose.se>
2026-04-20 09:47:18 +0200

    Simplify Run-Tests.ps1

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,30 +1,22 @@
-$sharpmakecs = "$PSScriptRoot/tests/sharpmake.cs".Replace('\', '/')
-& "$PSScriptRoot/tools/Generate-Sharpmake.ps1" -TagName 1.3.0 -Params "/sources('$sharpmakecs')"
+& "./tools/Generate-Sharpmake.ps1" -TagName 1.3.0 -Params "/sources('./tests/sharpmake.cs')"
 
 if ($LASTEXITCODE -ne 0) {
 	Exit($LASTEXITCODE)
 }
 
-Push-Location -Path "$PSScriptRoot/build"
-
 if (Test-Path "./output") {
 	Remove-Item "./output" -Force -Recurse
 }
 
-Start-Job -ScriptBlock {
-	$PSScriptRoot = "$using:PSScriptRoot"
-	& "$PSScriptRoot/tools/Launch-VsDevShell.ps1" -Lates
-	$slnlist = Get-ChildItem "$PSScriptRoot/build/" -Filter "*.sln" -Recurse
-	foreach ($sln in $slnlist) {
-		msbuild -restore $sln.FullName /nologo /verbosity:m /p:Configuration="Release" /maxcpucount
-	}
+& "./tools/Launch-VsDevShell.ps1" -Lates
+# Launching VsDevShell loses the working directory, so set it back to the script location.
+Push-Location -Path $PSScriptRoot
+$slnlist = Get-ChildItem "./build/" -Filter "*.sln"
+foreach ($sln in $slnlist) {
+	msbuild $sln.FullName /verbosity:m /p:Configuration="Release" /maxcpucount
+}
 
-} | Receive-Job -Wait -AutoRemoveJob
-
-Push-Location -Path "./output"
-$exelist = Get-ChildItem "./" -Filter "*.exe" -Recurse
+$exelist = Get-ChildItem "./build/output" -Filter "*.exe" -Recurse
 foreach ($exe in $exelist) {
 	& $exe.FullName
 }
-Pop-Location
-Pop-Location
